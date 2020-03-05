e6679d4b05105925fbe9dae70607104fb7b45167
Dimitra Ntzioni <dntzioni@gmail.com>
2020-03-05 15:48:19 +0200

    pagination feature

--- config.py
+++ config.py
@@ -9,10 +9,13 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'microblog_app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    #Email Configuration
+    # Email Configuration
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
+
+    # Posts per page configuration.
+    POSTS_PER_PAGE = 10
