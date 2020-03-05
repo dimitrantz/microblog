c7ec36b41751c64b37b6b11904a1fbdb218ccce5
Dimitra Ntzioni <dntzioni@gmail.com>
2020-03-05 13:15:02 +0200

    registration feature

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 flaskapp = Flask(__name__)
 flaskapp.config.from_object(Config)
 db = SQLAlchemy(flaskapp)
 migrate = Migrate(flaskapp, db)
+
+login = LoginManager(flaskapp)
+login.login_view = 'login'
 
 from app import routes, models
