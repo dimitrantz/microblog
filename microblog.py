556a4ffeb429f1cb653cd2aed1c56b1b0d0529ba
Dimitra Ntzioni <dntzioni@gmail.com>
2020-03-05 12:25:42 +0200

    update microblog.py

--- microblog.py
+++ microblog.py
@@ -1,1 +1,6 @@
-from app import flaskapp
+from app import flaskapp, db
+from app.models import User, Post
+
+@flaskapp.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
