bc5934b0dc7574bcbe744d8ab8f658f4626c8561
Dimitra Ntzioni <dntzioni@gmail.com>
2020-03-05 14:07:25 +0200

    error handling feature

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 flaskapp = Flask(__name__)
 flaskapp.config.from_object(Config)
 db = SQLAlchemy(flaskapp)
@@ -12,4 +16,34 @@ migrate = Migrate(flaskapp, db)
 login = LoginManager(flaskapp)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
+
+if not flaskapp.debug:
+    if flaskapp.config['MAIL_SERVER']:
+        auth = None
+        if flaskapp.config['MAIL_USERNAME'] or flaskapp.config['MAIL_PASSWORD']:
+            auth = (flaskapp.config['MAIL_USERNAME'], flaskapp.config['MAIL_PASSWORD'])
+        secure = None
+        if flaskapp.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(flaskapp.config['MAIL_SERVER'], flaskapp.config['MAIL_PORT']),
+            fromaddr='no-reply@' + flaskapp.config['MAIL_SERVER'],
+            toaddrs=flaskapp.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        flaskapp.logger.addHandler(mail_handler)
+
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                        backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        flaskapp.logger.addHandler(file_handler)
+
+        flaskapp.logger.setLevel(logging.INFO)
+        flaskapp.logger.info('Microblog startup')
