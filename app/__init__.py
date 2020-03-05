480db0242d50c966192eca4cac31909ddab27586
Dimitra Ntzioni <dntzioni@gmail.com>
2020-03-05 11:59:21 +0200

    database update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 flaskapp = Flask(__name__)
 flaskapp.config.from_object(Config)
+db = SQLAlchemy(flaskapp)
+migrate = Migrate(flaskapp, db)
 
-
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'try-to-find-me'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'microblog_app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
